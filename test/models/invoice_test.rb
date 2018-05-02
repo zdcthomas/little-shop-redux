852dd973c021dd586efeb8f74a2f2f468bd50810
zdcthomas <zdcthomas@yahoo.com>
2018-05-02 08:38:44 -0600

    fixes typo in invoice_test

--- test/models/invoice_test.rb
+++ test/models/invoice_test.rb
@@ -4,7 +4,7 @@ require './app/models/invoice'
 
 class InvoiceTest < Minitest::Test
   def test_it_exists
-    invoice = Invoice.new(id: 1, customer_id: 1, merchant_id: 12335938, status: 'pending',)
+    invoice = Invoice.new(id: 1, customer_id: 1, merchant_id: 12335938, status: 'pending')
     assert_instance_of Invoice, invoice
   end
 end
