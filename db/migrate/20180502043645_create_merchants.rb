683f8aef69df631c27a4285ff5bc3298e02196b7
Eliot Swank <eliotswank@gmail.com>
2018-05-02 08:34:29 -0600

    WIP merchants db

--- db/migrate/20180502043645_create_merchants.rb
+++ db/migrate/20180502043645_create_merchants.rb
@@ -1,4 +1,9 @@
 class CreateMerchants < ActiveRecord::Migration[5.2]
   def change
+    create_table :merchants do |t|
+      t.integer :id
+      t.text :name
+
+      t.timestamps null: false
   end
 end
