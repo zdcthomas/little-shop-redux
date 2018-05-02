cac74d9fe7eef772006d6342bcf657bc3bfc337a
Eliot Swank <eliotswank@gmail.com>
2018-05-01 22:37:31 -0600

    creates db

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,9 +1,12 @@
-Rspec.describe 'validations' do
-  describe 'required fields' do
-    it 'should be invalid if there is no name' do
-      merchant = Merchant.create(id: 1234567, created_at: '2010-12-10')
+RSpec.describe Merchant do
 
-      expect(merchant).to_not be_valid
+  describe 'validations' do
+    describe 'required fields' do
+      it 'should be invalid if there is no name' do
+        merchant = Merchant.create(id: 1234567)
+
+        expect(merchant).to_not be_valid
+      end
     end
   end
 end
