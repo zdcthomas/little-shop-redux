1d04bd5afa2e81be857f016004f1eab923b6acbd
Eliot Swank <eliotswank@gmail.com>
2018-05-02 08:49:45 -0600

    Implements Merchant

--- db/migrate/20180502043645_create_merchants.rb
+++ db/migrate/20180502043645_create_merchants.rb
@@ -1,9 +1,10 @@
 class CreateMerchants < ActiveRecord::Migration[5.2]
   def change
     create_table :merchants do |t|
-      t.integer :id
+      t.integer :merchant_id
       t.text :name
 
       t.timestamps null: false
+    end
   end
 end
