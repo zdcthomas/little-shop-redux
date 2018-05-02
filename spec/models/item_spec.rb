c203d5966f97a23b2298d6c6ffe138132545ac4f
JamisonOrdway <jamisonordway@gmail.com>
2018-05-01 19:41:04 -0600

    TESTS item valid contexts

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,11 @@
+Rspec.describe Item do
+
+  describe 'validations' do
+    describe 'valid context' do
+      it 'should be valid with a title, description, and price'
+      item = Item.create(title: 't-shirt', description: 'you can wear it if you want', price: 10.00)
+
+      expect(item).to be_valid
+    end
+  end
+end
