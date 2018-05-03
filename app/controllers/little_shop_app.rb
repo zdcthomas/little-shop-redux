c758c379598dcc4ee07d3513bd506c281639775c
zdcthomas <zdcthomas@yahoo.com>
2018-05-03 14:18:07 -0600

    implements shows specific invoice

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -3,4 +3,17 @@ class LittleShopApp < Sinatra::Base
     @invoices = Invoice.all
     erb :"invoices/index"
   end
+
+  get '/invoices/:id' do
+    if Invoice.exists?(params[:id])
+      @invoice = Invoice.find(params[:id])
+      erb :'invoices/show'
+    else
+      redirect not_found
+    end
+  end
+
+  not_found do
+    status 404
+  end
 end
--- spec/features/invoices/user_sees_invoices_spec.rb
+++ spec/features/invoices/user_sees_invoices_spec.rb
@@ -19,14 +19,15 @@ RSpec.describe 'A User' do
     it 'should display a certain invoice if that invoice exists' do
       Invoice.create(id: 1, merchant_id: 12335938, customer_id: 1, status: 'pending')
       visit '/invoices/1'
-      expect(page).to_not have_content('No Such Invoice')
-      expect(page).to have_content('pending')
-      expect(page).to have_content('12335938')
+      expect(page).to have_content('Status: pending')
+      expect(page).to have_content('Merchant Id: 12335938')
+      expect(page).to have_content('Customer Id: 1')
+
     end
 
     it 'should display no such invoice if invoice doesnt exist' do
       visit '/invoices/1'
-      expect(page).to have_content('No Such Invoice')
+      expect(page.status_code).to eq(404)
     end
   end
 end
