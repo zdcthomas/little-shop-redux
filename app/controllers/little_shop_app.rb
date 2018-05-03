b1a6080d4ed1faaba36619419bb63bf03d89b1e2
zdcthomas <zdcthomas@yahoo.com>
2018-05-03 16:38:32 -0600

    implements edit in controller

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -13,7 +13,22 @@ class LittleShopApp < Sinatra::Base
     end
   end
 
+  get '/invoices/:id/edit' do
+    if Invoice.exists?(params[:id])
+      @invoice = Invoice.find(params[:id])
+      erb :'invoices/edit'
+    else
+      redirect not_found
+    end
+
+  post '/invoices' do
+    invoice = Invoice.new(params[:invoice])
+    invoice.save
+    redirect '/invoices'
+  end
+
   not_found do
     status 404
   end
+
 end
