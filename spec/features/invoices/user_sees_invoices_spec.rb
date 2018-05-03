ef31c629409cd39884938772b97680077f6e7d23
zdcthomas <zdcthomas@yahoo.com>
2018-05-03 13:50:24 -0600

    TESTS individual invoice display

--- spec/features/invoices/user_sees_invoices_spec.rb
+++ spec/features/invoices/user_sees_invoices_spec.rb
@@ -13,6 +13,20 @@ RSpec.describe 'A User' do
       expect(page).to have_content('confirmed')
       expect(page).to have_content('14839458')
     end
+  end
 
+  describe 'visits a specific invoice' do
+    it 'should display a certain invoice if that invoice exists' do
+      Invoice.create(id: 1, merchant_id: 12335938, customer_id: 1, status: 'pending')
+      visit '/invoices/1'
+      expect(page).to_not have_content('No Such Invoice')
+      expect(page).to have_content('pending')
+      expect(page).to have_content('12335938')
+    end
+
+    it 'should display no such invoice if invoice doesnt exist' do
+      visit '/invoices/1'
+      expect(page).to have_content('No Such Invoice')
+    end
   end
 end
