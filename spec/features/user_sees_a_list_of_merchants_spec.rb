6cf300cffbbc7f3f358d8a8ab68ded5fc3257202
Eliot Swank <eliotswank@gmail.com>
2018-05-03 13:22:25 -0600

    Implements feature test for /merchants--has a name

--- spec/features/user_sees_a_list_of_merchants_spec.rb
+++ spec/features/user_sees_a_list_of_merchants_spec.rb
@@ -1,9 +1,9 @@
 RSpec.describe 'user visits merchant index page' do
   it 'should see a list of merchants' do
-    merchant_name = Merchant.create(name: "Merchant 1")
+    merchant = Merchant.create(name: 'Merchant 1')
 
     visit('/merchants')
 
-    expect(page).to have_content(merchant_name)
+    expect(page).to have_content(merchant.name)
   end
 end
