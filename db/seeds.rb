4427437f035fd263e02c2b964f613534fdfde78f
JamisonOrdway <jamisonordway@gmail.com>
2018-05-07 11:25:08 -0600

    Implements iteration 3 fixes unit_price in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ CSV.foreach('./data/items.csv', headers: true, header_converters: :symbol) do |i
       Item.create(id: i[:id],
                   title: i[:title],
                   description: i[:description],
-                  unit_price: i[:price],
+                  price: i[:unit_price],
                   img: i['https://i.imgur.com/47sZdlE.jpg'],
                   merchant_id: i[:merchant_id],
                   created_at: i[:created_at],
