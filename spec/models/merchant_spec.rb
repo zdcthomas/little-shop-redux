d2ad4907916f9a85b323522ce38b3fb171734425
zdcthomas <zdcthomas@yahoo.com>
2018-05-08 14:01:25 -0600

    TESTS merchant method all_items

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe Merchant do
       end
     end
   end
+  describe "class methods" do
+    describe "all_items" do
+      it 'should return and array of all items associated with a merchant' do
+        Merchant.create(name: 'Shopin1901')
+        item1 = Item.create(title: 't-shirt', description: 'you can wear it if you want', unit_price: 1000, img: './default-photo.png', merchant_id: 1)
+        item2 = Item.create(title: 'another item', description: 'you can wear it if you want', unit_price: 1000, img: './default-photo.png', merchant_id: 1)
+        item3 = Item.create(title: 'wait, no, another item', description: 'you can wear it if you want', unit_price: 1000, img: './default-photo.png', merchant_id: 1)
+
+        expect(Merchant.all_items(1)).to eq([item1, item2, item3])
+      end
+    end
+  end
 end
