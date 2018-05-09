59bdef7d33c47ec430fd465a0b4e3eebc6345407
zdcthomas <zdcthomas@yahoo.com>
2018-05-09 09:11:46 -0600

    fixes item edit bug

--- spec/features/items/user_can_edit_existing_items_spec.rb
+++ spec/features/items/user_can_edit_existing_items_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe 'Visitors' do
     item1_price = 7
     item1_image = '/images/default-photo.png'
     item1 = Item.create(name: item1_name, description: item1_description, unit_price: item1_price, img: item1_image)
-    item1.save
     visit('/items/1/edit')
 
-    fill_in 'item[name]', :with=> 'thing'
-    fill_in 'item[description]', :with=> 'information'
-    fill_in 'item[unit_price]', :with=> 6
-    fill_in 'item[img]', :with=> 'http://www.suttonsilver.co.uk/wp-content/uploads/blog-stock-02.jpg'
-    item1.save
+    fill_in('item[name]', :with=> 'thing')
+    fill_in('item[description]', :with=> 'information')
+    fill_in('item[unit_price]', :with=> 6)
+    fill_in('item[img]', :with=> 'http://www.suttonsilver.co.uk/wp-content/uploads/blog-stock-02.jpg')
+    
     click_button('Submit')
+    expect(page.current_path).to eq('/items')
     expect(page).to have_content('thing')
   end
 end
